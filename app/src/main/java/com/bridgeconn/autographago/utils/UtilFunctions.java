7394b68958549ec641151883c8c652584298cf6b
Prerna Singh <prerna11082@iiitd.ac.in>
2017-01-05 18:50:50 +0530

    missed one file

--- app/src/main/java/com/bridgeconn/autographago/utils/UtilFunctions.java
+++ app/src/main/java/com/bridgeconn/autographago/utils/UtilFunctions.java
@@ -1,9 +1,13 @@
 package com.bridgeconn.autographago.utils;
 
+import android.app.Activity;
 import android.content.Context;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
 import org.apache.commons.io.IOUtils;
@@ -11,8 +15,10 @@ import org.apache.commons.io.IOUtils;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.util.Enumeration;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -23,5 +29,50 @@ import java.util.zip.ZipFile;
 
 public class UtilFunctions {
 
+    public static void pullDB(String sDBName) {
+        try {
+            File sd = Environment.getExternalStorageDirectory();
+            File data = Environment.getDataDirectory();
+            if (sd.canWrite()) {
+                String currentDBPath = "data/"+"com.bridgeconn.autographago"+"/files/"+ sDBName;
+                String backupDBPath = "/appname-external-data-cache/"+sDBName; //"{database name}";
+                File dir = new File(sd,backupDBPath.replace(sDBName,""));
+                if(dir.mkdir()) {
+
+                }
+                File currentDB = new File(data, currentDBPath);
+                File backupDB = new File(sd, backupDBPath);
+                if (currentDB.exists()) {
+                    FileChannel src = new FileInputStream(currentDB).getChannel();
+                    FileChannel dst = new FileOutputStream(backupDB).getChannel();
+                    dst.transferFrom(src, 0, src.size());
+                    src.close();
+                    dst.close();
+                }
+            } else {
+                Log.e("abcd", "sd cannot write");
+            }
+        } catch (Exception e) {
+            Log.e("abcd", e.toString());
+        }
+    }
+
+    public static void hideKeyboard(Activity activity, View view) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (view == null) {
+            view = activity.getCurrentFocus();
+        }
+        if (view != null) {
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    public static void showKeyboard(Activity activity, View view) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (view == null) {
+            view = activity.getCurrentFocus();
+        }
+        imm.showSoftInput(view, InputMethodManager.SHOW_FORCED);
+    }
 
 }
