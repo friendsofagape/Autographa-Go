ce86a3bb169f16f65052a5e79c2414fcaa9257c3
Prerna Singh <prerna11082@iiitd.ac.in>
2017-07-04 19:29:52 +0530

    missed one file

--- app/src/main/java/com/bridgeconn/autographago/ui/viewholders/BookItemViewHolder.java
+++ app/src/main/java/com/bridgeconn/autographago/ui/viewholders/BookItemViewHolder.java
@@ -8,12 +8,25 @@ import android.widget.TextView;
 
 import com.bridgeconn.autographago.R;
 import com.bridgeconn.autographago.models.BookIdModel;
+import com.bridgeconn.autographago.models.BookModel;
+import com.bridgeconn.autographago.models.ChapterModel;
+import com.bridgeconn.autographago.models.VerseIdModel;
+import com.bridgeconn.autographago.ormutils.AllSpecifications;
+import com.bridgeconn.autographago.services.BackgroundService;
+import com.bridgeconn.autographago.ui.activities.BookActivity;
 import com.bridgeconn.autographago.ui.activities.SelectChapterAndVerseActivity;
 import com.bridgeconn.autographago.utils.Constants;
+import com.bridgeconn.autographago.utils.SharedPrefs;
+import com.bridgeconn.autographago.utils.UtilFunctions;
 
 import java.util.ArrayList;
 
-public class BookItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+import io.realm.Realm;
+import io.realm.RealmResults;
+
+import static android.app.Activity.RESULT_OK;
+
+public class BookItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
 
     private TextView mTvBookName;
     private View mView;
@@ -35,6 +48,7 @@ public class BookItemViewHolder extends RecyclerView.ViewHolder implements View.
         mTvBookName.setText(bookModel.getBookName());
         mView.setTag(position);
         mView.setOnClickListener(this);
+        mView.setOnLongClickListener(this);
 //        if (position == 0 || position == 39) {
 //            mTvHeader.setVisibility(View.VISIBLE);
 //        } else {
@@ -56,5 +70,56 @@ public class BookItemViewHolder extends RecyclerView.ViewHolder implements View.
                 break;
             }
         }
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        switch (v.getId()) {
+            case R.id.root_item: {
+                int position = (int) v.getTag();
+
+                String languageCode = SharedPrefs.getString(Constants.PrefKeys.LAST_OPEN_LANGUAGE_CODE, "ENG");
+                String versionCode = SharedPrefs.getString(Constants.PrefKeys.LAST_OPEN_VERSION_CODE, Constants.VersionCodes.UDB);
+                String bookId = mBookModels.get(position).getBookId();
+
+                Realm realm = Realm.getDefaultInstance();
+                String verseNumber = getVerseNumber(realm, languageCode, versionCode, bookId, 1);
+                realm.close();
+
+                Intent task = new Intent(mContext, BackgroundService.class);
+                task.setAction(Constants.ACTION.ADD_TO_HISTORY);
+                task.putExtra(Constants.Keys.VERSE_NO, verseNumber);
+                task.putExtra(Constants.Keys.CHAPTER_NO, 1);
+                task.putExtra(Constants.Keys.LANGUAGE_CODE, languageCode);
+                task.putExtra(Constants.Keys.VERSION_CODE, versionCode);
+                task.putExtra(Constants.Keys.BOOK_ID, bookId);
+                task.putExtra(Constants.Keys.TIMESTAMP, System.currentTimeMillis());
+                mContext.startService(task);
+
+                Intent intent = new Intent(mContext, BookActivity.class);
+                intent.putExtra(Constants.Keys.BOOK_ID, bookId);
+                intent.putExtra(Constants.Keys.CHAPTER_NO, 1);
+                intent.putExtra(Constants.Keys.VERSE_NO, verseNumber);
+                mContext.startActivity(intent);
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String getVerseNumber(Realm realm, String languageCode, String versionCOde, String bookId, int chapterNum) {
+        RealmResults<BookModel> realmResults = new AllSpecifications.BookModelById(languageCode, versionCOde, bookId).generateResults(realm);
+        if (realmResults.size() > 0) {
+            for (ChapterModel chapterModel : realmResults.get(0).getChapterModels()) {
+                if (chapterModel.getChapterNumber() == chapterNum) {
+                    if (chapterModel.getVerseComponentsModels().size() > 0) {
+                        String verseNumber = chapterModel.getVerseComponentsModels().get(0).getVerseNumber();
+                        return verseNumber;
+                    }
+                }
+            }
+        }
+        return "1";
     }
 }
